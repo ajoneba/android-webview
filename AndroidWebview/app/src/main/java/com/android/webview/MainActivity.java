e3bcd793820615e4b0aeb076a78ee5266c31fda3
ajoneba <33817815+ajoneba@users.noreply.github.com>
2020-08-07 23:07:17 +0900

    Added base code for android webview template.

---
+++ AndroidWebview/app/src/main/java/com/android/webview/MainActivity.java
@@ -0,0 +1,59 @@
+package com.android.webview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView = null;
+    private String url;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        url = "https://www.octopart.com";
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.setWebViewClient(new SSLTolerantWebViewClient());
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(url);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        webView.restoreState(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+}
---
+++ AndroidWebview/app/src/main/java/com/android/webview/SSLTolerantWebViewClient.java
@@ -0,0 +1,12 @@
+package com.android.webview;
+
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class SSLTolerantWebViewClient extends WebViewClient {
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed();
+    }
+}
